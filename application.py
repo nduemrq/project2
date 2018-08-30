051e7a7afddc8013812c9f1f917ab2a596fdbc06
nduemrq <gitmrq@wir.pl>
2018-08-30 22:35:02 +0200

    recive and send data when select channel name

--- application.py
+++ application.py
@@ -3,6 +3,10 @@ import os
 from flask import Flask, render_template, session
 from flask_socketio import SocketIO, emit
 
+# olny for debug mode
+from gevent import monkey
+monkey.patch_all()
+
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
@@ -23,3 +27,11 @@ def checkNewChannel(data):
     else:
         channelList.append(channelName)
         emit('channel new', data, broadcast=True)
+
+
+@socketio.on("channel selected")
+def channelSelected(data):
+    ''' When user selecteed one of channel list then
+        thist function receive channel name.
+        Need to return channel message. '''
+    emit('channel message', data)
